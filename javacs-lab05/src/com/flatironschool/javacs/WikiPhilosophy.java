cb55e5f356ad87eb3da6a895bea7225f38336652
CeciliaLiu <ziyue.liu7@gmail.com>
2016-06-04 20:46:34 -0500

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -30,22 +30,74 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
+        List<String> pagesVisited = new ArrayList<String>();
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		String target = "https://en.wikipedia.org/wiki/Philosophy";
+		boolean isLink = false;
+		while (!url.equals(target)) {
+			Elements paragraphs = wf.fetchWikipedia(url);
+			pagesVisited.add(url);
+			int parenthesis = 0;
+			for (Element firstPara : paragraphs) {
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+				Iterable<Node> iter = new WikiNodeIterable(firstPara);
+				for (Node node: iter) {
+					if (node instanceof TextNode) {
+						if (((TextNode) node).text().contains("(")) {
+							parenthesis++;
+						} else if (((TextNode) node).text().contains(")")) {
+							parenthesis--;
+						}
+					} else if (node instanceof Element) {
+						String absUrl = ((Element) node).absUrl("href");
+						Element e = (Element) node;
+						if (e.tagName() == "a") {
+							// found a link 
+							isLink = true;
+							// Check if it contains italics
+							for (Element ele : e.parents()) {
+								if (ele.tagName().equals("i") || ele.tagName().equals("em")) {
+									isLink = false;
+								}
+							}
+							// If parenthesis is not valid, the link is not valid
+							if (parenthesis != 0) {
+								isLink = false;
+							}
+
+							// Find next
+							String next_url = "https://en.wikipedia.org" + node.attr("href");
+							int end = next_url.indexOf('#');
+							if (end != -1 || pagesVisited.contains(next_url)) {
+								isLink = false;
+							}
+
+							if (isLink) {
+								url = next_url;
+								break;
+							}
+
+						}
+
+					}
+
+				}
+				if (isLink) {
+					break;
+				}
 			}
-        }
+			if (!isLink) {
+				System.out.println("Link not found");
+				break;
+			}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		}
+
+        // // the following throws an exception so the test fails
+        // // until you update the code
+        // String msg = "Complete this lab by adding your code and removing this statement.";
+        // throw new UnsupportedOperationException(msg);
 	}
+
 }
